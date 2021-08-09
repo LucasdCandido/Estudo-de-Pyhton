1b68adb70387b1ac33557ec89494740a3c74fdb6
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-09 11:03:37 -0300

    main

--- Exercicios/Mundo 2 PY/exercicio 26.py
+++ Exercicios/Mundo 2 PY/exercicio 26.py
@@ -1,18 +1,19 @@
-termo = int(input('Digite o primeiro termo da PA: '))
-razao = int(input('Digite a razao do termo da PA: '))
-t = termo
-c = 1
+primeiro = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+termo = primeiro
+cont = 1
+cont2 = 1
 n = 1
-while c != 0:
-    while c <= 10:
-        print('{} --> '.format(t), end="")
-        t += termo + razao
-        c += 1
-        n += 1
+while n !=0:
+    while cont < 10:
+        print('{} -->'.format(termo), end="")
+        termo = primeiro + razao
+        cont += 1
+        cont2 += 1
     print('PAUSA')
-    c = int(input('Quantos termos a mais gostaria de ver? '))
-print('Foram apresentados {} termos da PA'.format(n))
-
+    n = int(input('Quantos termos a mais você gostaria de ver? '))
+    cont = 10 - n
+print('Foram mostrado {} termos da PA.'.format(cont2))
 
 
 
