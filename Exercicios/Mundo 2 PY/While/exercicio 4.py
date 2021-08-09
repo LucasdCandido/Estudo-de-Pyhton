e189fe2d3370f5b8ee02ab650dfb28737e1380a3
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-09 14:18:43 -0300

    Exercicio de while relacionado a crescimento populacional

--- Exercicios/Mundo 2 PY/While/exercicio 4.py
+++ Exercicios/Mundo 2 PY/While/exercicio 4.py
@@ -1,18 +1,23 @@
-cidA = int(input('População do pais A: '))
-while cidA <0:
-    cidA = int(input('População do pais A: '))
-taxaA=float(input('Taxa de crescimento da populção: '))
-cidB = int(input('População do pais B: '))
+cidA = float(input('Digite o valor da população da cidade A: '))
+while cidA < 0:
+    cidA = float(input('Digite um valor valido da população da cidade A: '))
+taxa_A = float(input('Digite o valor da taxa de crescimento da cidade A: '))
+while taxa_A < 0:
+    taxa_A = float(input('Digite uma taxa de crescimento valida: '))
+cidB = float(input('Digite a população da cidade B: '))
 while cidB < 0:
-    cidB = int(input('População do pais B: '))
-taxaB=float(input('Taxa de crescimento da cidade B: '))
-tA = taxaA /100
-tB = taxaB /100
+    cidB = float(input('Digite um valor valido da popupação da cidade B: '))
+taxa_B = float(input('Digite o valor da taxa de crescimento da cidade B: '))
+while taxa_B < 0:
+    taxa_B = float(input('Digite uma taxa de crescimento valida: '))
 ano = 0
-while cidA <= cidB:
-    cidA = cidA * tA
-    cidB = cidB * tB
-    ano = +1
+while cidA < cidB:
+    ano += 1
+    cidA = float((1 + (taxa_A/100))* cidA)
+    cidB = float((1 + (taxa_B/100))* cidB)
+    print('{:.2f}'.format(cidA))
+    print('{:.2f}'.format(cidB))
+print('Após {} anos, a cidade A ultrapassou a população da cidade B.'.format(ano))
 
 
 
