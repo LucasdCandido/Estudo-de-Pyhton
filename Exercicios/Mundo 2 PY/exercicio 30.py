cdc538aacb2a1799f46fdd91446f53389c053816
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-10 21:27:21 -0300

    Nova tentativa de corrigir o problema

--- Exercicios/Mundo 2 PY/exercicio 30.py
+++ Exercicios/Mundo 2 PY/exercicio 30.py
@@ -1,11 +1,13 @@
-s = cont = media = numero = 0
+s = cont = media = numero = n = c = 0
 f = 'S'
 while f in 'S':
-    s += numero
     numero = int(input('Digite um numero inteiro para tirar a media: '))
+    s = s + numero
     cont += 1
     f = str(input('Você gostaria de digitar mais um numero? [N/S] ')).upper().strip()[0]
-    media = numero / cont
+n = numero
+c = cont
+media = n / c
 print('Foram digitados {} numeros e a media entre eles é de {}'.format(cont, media))
 
 
