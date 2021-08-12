94f0fc3cd4f671e82559ced623dd2049a10760d9
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-12 19:18:15 -0300

    Exercicio de while com break tabuada de varios numeros diferentes

--- Exercicios/Mundo 2 PY/exercicio 32.py
+++ Exercicios/Mundo 2 PY/exercicio 32.py
@@ -1,4 +1,15 @@
-
+tabuada = numero = cont = 0
+while True:
+    numero = int(input('Digite um numero para ver a tabuada: '))
+    if numero < 0:
+        break
+    while True:
+        cont += 1
+        print(f'{numero} x {cont} = {numero*cont}')
+        if cont>=10:
+            break
+    cont = 0
+print('FIM')
 
 
 
