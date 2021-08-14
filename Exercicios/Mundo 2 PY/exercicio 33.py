2667bb515b53152d17fb4d33b3de5cc27c20db0f
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-13 22:04:40 -0300

    Finalização do exercicio de while com break jogando par ou impar com o computador

--- Exercicios/Mundo 2 PY/exercicio 33.py
+++ Exercicios/Mundo 2 PY/exercicio 33.py
@@ -1,24 +1,40 @@
 from random import randint
-computador = jogador = contador = soma = resto = 0
+computador = jogador = contador = soma = resto = par = impar = resultado = 0
 escolha = ""
-par = 0
-impar = 0
-resultado = 0
 print('=-='*30)
 print('''Vamos jogar par ou impar?
     Você vai competir com o computador, vamos ver quem tem mais sorte! ''')
+print('=-='*30)
 while True:
     computador = randint(1,10)
     escolha = str(input('Você gostaria de escolher PAR ou IMPAR? ')).upper().strip()
-    while escolha not in 'PAR' or 'IMPAR':
+    while escolha not in 'PARIMPAR':
         escolha = str(input('Por favor escolha entre PAR ou IMPAR: '))
     if escolha == 'PAR':
         jogador = int(input('Digite um numero para jogar: '))
         soma = computador + jogador
-        contador += 1
         resto = soma % 2
         if resto == 0:
-            
+            print(f'Parabéns você venceu, você escolheu o numero {jogador} e o computador escolheu o numero {computador} somando da {soma} que é PAR!')
+            resultado = 0
+            contador += 1
+        elif resto != 0:
+            print(f'Infelizmente você perdeu, você escolheu {jogador} e o computador escolheu {computador} somando da {soma} que é IMPAR!')
+            resultado = 1
+    elif escolha == 'IMPAR':
+        jogador = int(input('Digite um numero para jogar: '))
+        soma = computador + jogador
+        resto = soma % 2
+        if resto != 0:
+            print(f'Parabéns você venceu, você escolheu o numero {jogador} e o computador escolheu o numero {computador} somando da {soma} que é IMPAR!')
+            resultado = 0
+            contador += 1
+        elif resto == 0:
+            print(f'Infelizmente você perdeu, você escolheu {jogador} e o computador escolheu {computador} somando da {soma} quie é PAR!')
+            resultado = 1
+    if resultado == 1:
+        break
+print(f'Você venceu {contador} vezes!')
 
 
 
