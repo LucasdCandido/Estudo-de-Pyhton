030a6adee07cafc5fc8f913b51c34aadc915625f
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-15 01:20:18 -0300

    Exercicio de while com break cadastro de pessoas

--- Exercicios/Mundo 2 PY/exercicio 34.py
+++ Exercicios/Mundo 2 PY/exercicio 34.py
@@ -1,3 +1,29 @@
+nome = sexo = nomes = sexos = escolha = ""
+idade = letra = idades = 0
+
+
+while True:
+    nome = str(input('Digite seu nome: ')).upper().strip()
+    letra =len(nome)
+    while letra < 3:
+        nome = str(input('Por favor, digite um nome maior que 3 letras: '))
+        letra = len(nome)
+    idade = int(input('Digite sua idade: '))
+    while idade < 0:
+        idade = int(input('Digite um valor valido de idade: '))
+    sexo = str(input('Digite seu sexo: [M/F] ')).upper().strip()[0]
+    while sexo not in 'MmFf':
+        sexo = str(input('Por favor digite um sexo valido: [M/F] ')).upper().strip()[0]
+    nomes = nome
+    idades = idade
+    sexos = sexo
+    escolha = str(input('Gostaria de cadastrar outra pessoa? [S/N] ')).upper().strip()[0]
+    if escolha == 'N':
+        break
+print(nomes,idades,sexos)
+
+
+
 
 
 
