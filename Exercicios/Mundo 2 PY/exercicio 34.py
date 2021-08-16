c3251586242f7926d9770f410ddf2cf358267f50
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-16 12:10:27 -0300

    exercicio de while com break cadastro de pessoas e coleta de dados

--- Exercicios/Mundo 2 PY/exercicio 34.py
+++ Exercicios/Mundo 2 PY/exercicio 34.py
@@ -1,29 +1,23 @@
-nome = sexo = escolha = ""
-idade = letra = 0
-cadastro = []
-
+idade = contador18 = contadorm = contador18f = 0
+sexo = escolha = ""
 while True:
-    nome = str(input('Digite seu nome: ')).upper().strip()
-    letra =len(nome)
-    while letra < 3:
-        nome = str(input('Por favor, digite um nome maior que 3 letras: '))
-        letra = len(nome)
-    idade = int(input('Digite sua idade: '))
+    idade = int(input('Digite a sua idade: '))
     while idade < 0:
-        idade = int(input('Digite um valor valido de idade: '))
+        idade = int(input('Digite sua idade: '))
     sexo = str(input('Digite seu sexo: [M/F] ')).upper().strip()[0]
-    while sexo not in 'MmFf':
-        sexo = str(input('Por favor digite um sexo valido: [M/F] ')).upper().strip()[0]
-    cadastro = [nome, idade, sexo]
-    escolha = str(input('Gostaria de cadastrar outra pessoa? [S/N] ')).upper().strip()[0]
+    while sexo not in 'MF':
+        sexo = str(input('Digite seu sexo: [M/F] ')).upper().strip()[0]
+    if idade > 18:
+        contador18 += 1
+    if sexo == 'M':
+        contadorm += 1
+    if idade < 18 and sexo == 'F':
+        contador18f += 1
+    escolha = str(input('Você gostaria de continuar? [S/N] ')).upper().strip()[0]
     if escolha == 'N':
         break
-
-print(cadastro)
-
-
-
-
+print(f'Foram registrados {contadorm} homens, {contador18f} mulheres com menos de 18 anos e {contador18} pessoas com mais de 18 anos.')
+print('Esse é o fim do programa!')
 
 
 
