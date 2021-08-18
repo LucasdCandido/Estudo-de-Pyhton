23547e0561983fa223cb9bcc092ec2ff5883fc30
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-18 09:53:19 -0300

    Exercicio de while com break para a montagem de um caixa eletronico

--- Exercicios/Mundo 2 PY/exercicio 36.py
+++ Exercicios/Mundo 2 PY/exercicio 36.py
@@ -1,4 +1,26 @@
-
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
+valor = int(input('Que valor você quer sacar? R$ '))
+total = valor
+cedula = 50
+totalcedula = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalcedula += 1
+    else:
+        if totalcedula > 0:
+            print(f'Total de {totalcedula} cedulas de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalcedula = 0
+        if total == 0:
+            break
 
 
 
