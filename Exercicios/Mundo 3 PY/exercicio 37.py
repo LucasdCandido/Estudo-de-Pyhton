c37d8d91387e8ce247bfefa62ee2fb2f628d884b
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-18 11:56:18 -0300

    Exercicios de tuplas

---
+++ Exercicios/Mundo 3 PY/exercicio 37.py
@@ -0,0 +1,9 @@
+
+
+
+
+"""Crie um programa que tenha uma tupla totalmente preenchida com
+contagem por extrenso, de zero até vinte.
+Seu programa devera ler um numero pelo teclado (entre 0 e 20) e 
+mostra-lo por extenso.
+"""
---
+++ Exercicios/Mundo 3 PY/exercicio 38.py
@@ -0,0 +1,12 @@
+
+
+
+
+"""Crie uma tupla preenchida com os 20 primeiros colocados da 
+tabelo do campeonato brasileiro de futebol, na ordem de colocação.
+Depois mostre:
+A) Apenas os 5 primeiros colocados.
+B) Os ultimos 4 colocados da tabela.
+C) Uma lista com os times em ordem alfabetica.
+D) Em que posição na tabela esta o time Chapecoense.
+"""
