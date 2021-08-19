216c021db499494695e0303217ad30b7873c0dd2
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-19 13:21:30 -0300

    Exercicio de tupla com a classificação do brasileirão 08/2021

--- Exercicios/Mundo 3 PY/exercicio 38.py
+++ Exercicios/Mundo 3 PY/exercicio 38.py
@@ -1,6 +1,28 @@
-
-
-
+classificação = ('Atlético-MG', 'Palmeiras', 'Fortaleza', 'Bragantino', 'Flamengo', 'Athletico-PR', 'Atlético-GO', 'Ceará-SC', 'Internacional', 'Santos', 'Corinthians', 'Juventude', 'Bahia', 'São Paulo', 'Fluminense', 'Cuiabá', 'Sport Recife', 'América-MG', 'Grêmio', 'Chapecoense')
+n = c = m = contador = 0
+time = ""
+print(f'\nOs primeiros 5 colocados na tabela do Brasileirão são: \n')
+while n < 5:
+    print(classificação[n])
+    n += 1
+print(f'\n Os ultimos 4 colocados na tabela do Brasileirão são: \n')
+n = 16
+while n < 20:
+    print(classificação[n])
+    n += 1
+print(f'\nOs times que atualmente estão na Serie A do Brasileirão em ordem alfabetica são: \n')
+while True:
+    times = sorted(classificação)
+    print(f'{times[c]}'' ', end='')
+    c += 1
+    if c >= 19:
+        break
+for m in range(0, len(classificação), 1):
+    time = classificação[m]
+    contador += 1
+    if time == 'Chapecoense':
+        break
+print(f'\n\nO time Chapecoense esta classificado na posição {contador}!')
 
 """Crie uma tupla preenchida com os 20 primeiros colocados da 
 tabelo do campeonato brasileiro de futebol, na ordem de colocação.
