fecff7b68b31c08bdf1940bcbad87c3605b7691c
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-19 11:59:26 -0300

    Exercicio de tupla escrevendo numeros em extenso

--- Exercicios/Mundo 3 PY/exercicio 37.py
+++ Exercicios/Mundo 3 PY/exercicio 37.py
@@ -1,4 +1,11 @@
-
+numero = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    n = int(input('Digite um numero entre 0 e 20: '))
+    print(numero[n])
+    resposta = input('Gostaria de digitar mais um numero? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+print('Fim do programa!')
 
 
 
