0b7d92ec2da37251905f5ea1b9d88c8d9c2735b4
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-20 15:12:26 -0300

    Exercicios de tupla

--- Exercicios/Mundo 3 PY/exercicio 39.py
+++ Exercicios/Mundo 3 PY/exercicio 39.py
@@ -1,5 +1,23 @@
-
-
+from random import randint
+numero1 = numero2 = numero3 = numero4 = numero5 = random = contador = maior = menor = m = 0
+numero1 = randint(1, 10)
+numero2 = randint(1, 10)
+numero3 = randint(1, 10)
+numero4 = randint(1, 10)
+numero5 = randint(1, 10)
+random = (numero1, numero2, numero3, numero4, numero5)
+a, b, c, d, e = random
+for c in range(0, len(random), 1):
+    m = random[c]
+    contador += 1
+    if contador == 1:
+        maior = menor = m
+    else:
+        if m > maior:
+            maior = m
+        if m < menor:
+            menor = m
+print(f'A sequecia aleatoria ficou {a, b, c, d, e}, o maior numero da sequencia é {maior} e o menor é {menor}.')
 
 
 """Crie um programa que vai gerar cinco numeros aleatorios e colocar em uma tupla.
--- Exercicios/Mundo 3 PY/exercicio 41.py
+++ Exercicios/Mundo 3 PY/exercicio 41.py
@@ -1,3 +1,10 @@
+produtos = ('Sabão em Pó', 27.90, 'Leite', 4.87, 'Picanha', 37.90, 'Papel Aluminio', 3.90, 'Mouse', 17.99)
+a1, a2, b1, b2, c1, c2, d1, d2, e1, e2 = produtos
+print('=-='*30)
+titulo = 'LISTA DE COMPRA'
+print(titulo.center(80))
+print('=-='*30)
+print(f'{a1} ---------- R$ {a2}')
 
 
 
