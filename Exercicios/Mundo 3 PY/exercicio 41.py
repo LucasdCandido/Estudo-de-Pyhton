54a8c852f0bd7e908d1bd9c0ef6eadf443be2c03
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-23 14:25:48 -0300

    Sugestão de solução de exercicio

--- Exercicios/Mundo 3 PY/exercicio 41.py
+++ Exercicios/Mundo 3 PY/exercicio 41.py
@@ -1,5 +1,5 @@
-produtos = ('Sabão em Pó', 27.90, 'Leite', 4.87, 'Picanha', 37.90, 'Papel Aluminio', 3.90, 'Mouse', 17.99)
-a1, a2, b1, b2, c1, c2, d1, d2, e1, e2 = produtos
+produtos = ('Sabão em Pó', 27.99, 'Leite', 4.87, 'Picanha', 37.99, 'Papel Aluminio', 3.99, 'Mouse', 17.99)
+"""a1, a2, b1, b2, c1, c2, d1, d2, e1, e2 = produtos
 print('=-='*30)
 titulo = 'LISTA DE COMPRA'
 print(titulo.center(80))
@@ -9,8 +9,12 @@ print(f'''
                         {b1} ------------------- R$ {b2}
                         {c1} ----------------- R$ {c2}
                         {d1} ---------- R$ {d2}
-                        {e1} ------------------- R$ {e2}''')
-
+                        {e1} ------------------- R$ {e2}''')"""
+for posicao in range(0, len(produtos)):
+    if posicao % 2 == 0:
+        print(f'{produtos[posicao]:-<30} R$ ', end='')
+    else:
+        print(f'{produtos[posicao]:>6}')
 
 
 
