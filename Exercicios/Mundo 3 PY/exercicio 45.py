2532983fbbcfa1cae69f1cf5779544560a8f1033
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-25 16:59:03 -0300

    Tentativa de resolução de exercicio de lista

--- Exercicios/Mundo 3 PY/exercicio 45.py
+++ Exercicios/Mundo 3 PY/exercicio 45.py
@@ -1,4 +1,30 @@
-
+valores = []
+novos = []
+contador = maior = menor = maiormenor = maiormenor2 = maiormenor3 = 0
+for c in range(0,5):
+    valores.append(int(input('Digite um numero: ')))
+for v in valores:
+    contador += 1
+    if contador == 1:
+        maior = menor = menormenor = maiormenor2 = v
+        novos.append(v)
+    else:
+        if menor >= v:
+            menor = v
+            novos.lappend(menor)
+        if maior <= v:
+            maior = v
+            novos.append(maior)
+        if maiormenor > menor:
+            menormaior = v
+            novos.insert((novos.index(menor)), menormaior)
+        if maiormenor2 > maiormenor:
+            maiormenor2 = v
+            novos.insert((novos.index(maiormenor)), maiormenor2)
+        if maiormenor3 > maiormenor2:
+            maiormenor3 = v
+            novos.insert((novos.index(maiormenor2)), maiormenor3)
+print(novos)
 
 
 
