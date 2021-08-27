132c8c7ffad706c7124db73490a441375aadb071
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-27 12:13:20 -0300

    Resolução do exercicio de lista criando lista e dividindo em duas listas

--- Exercicios/Mundo 3 PY/exercicio 47.py
+++ Exercicios/Mundo 3 PY/exercicio 47.py
@@ -1,4 +1,21 @@
-
+valores = []
+par = []
+impar = []
+reposta = ""
+contadorpar = contadorimpar = 0
+while True:
+    valores.append(int(input('Digite um numero: ')))
+    resposta = input('Gostaria de continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+for v in valores:
+    if v % 2 == 0:
+        par.append(v)
+        contadorpar += 1
+    else:
+        impar.append(v)
+        contadorimpar += 1
+print(f'A lista de numeros pares é {par}\nA lista de numeros impares é {impar}\nForam digitados {contadorpar} numeros pares\nForam digitados {contadorimpar} numeros impares')
 
 
 
