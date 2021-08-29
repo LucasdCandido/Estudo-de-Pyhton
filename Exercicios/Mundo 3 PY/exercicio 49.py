424d2d1596948e4ea13c20592356b331e0006aa8
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-29 19:56:27 -0300

    Exercicios de lista de listas

--- Exercicios/Mundo 3 PY/exercicio 49.py
+++ Exercicios/Mundo 3 PY/exercicio 49.py
@@ -2,5 +2,8 @@
 
 
 
-"""
+"""Faça um programa que leia o nome e o peso de varias pessoas, guardando tudo em uma lista. no final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma lista com as pessoas mais leves.
 """
--- Exercicios/Mundo 3 PY/exercicio 51.py
+++ Exercicios/Mundo 3 PY/exercicio 51.py
@@ -2,5 +2,6 @@
 
 
 
-"""
+"""Crie um programa que crie uma matriz de dimenssão 3x3 e preencha com valores lidos pelo teclado:
+No final, mostre a matriz na tela, com a formatação correta.
 """
