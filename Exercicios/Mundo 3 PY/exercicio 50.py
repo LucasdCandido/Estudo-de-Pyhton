cc987ab52890302cebb59d94dbc6a707ad8f3c96
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-08-31 08:56:16 -0300

    Resolução de exercicio de lista de listas

--- Exercicios/Mundo 3 PY/exercicio 50.py
+++ Exercicios/Mundo 3 PY/exercicio 50.py
@@ -1,7 +1,20 @@
-
+valores = []
+par = []
+impar = []
+for c in range(0, 7):
+    valores.append(int(input('Digite um numero: ')))
+for v in valores:
+    if v % 2 == 0:
+        par.append(v)
+    else:
+        impar.append(v)
+par.sort()
+impar.sort()
+print(par)
+print(impar)
 
 
 
 """Crie um programa onde o usuario possa digitar sete valores numeros e cadastre-os em uma lista unica
-que mantenha separados os valores pares e impares. Nofinal, m,ostre os valores pares e impares em odem crescente.
+que mantenha separados os valores pares e impares. No final, mostre os valores pares e impares em odem crescente.
 """
