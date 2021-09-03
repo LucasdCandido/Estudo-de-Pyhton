8522275550bcd059ea3b2911a87ef9016a9f4675
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-09-03 17:03:51 -0300

    Necessidade especial

--- Exercicios/Mundo 2 PY/Trabalho/Salario.py
+++ Exercicios/Mundo 2 PY/Trabalho/Salario.py
@@ -4,7 +4,7 @@ inss = salario*0.075
 liquido = salario-vt-inss
 print('''O valor do salario liquido é de R$ {:.2f},
       o valor descontado de VT do salario é de R$ {:.2f} e o valor do inss é de R$ {:.2f}'''.format(liquido, vt, inss))
-pag = 1194.84 + 939
+pag = float(input('Digite quanto foi pago: R$ '))
 print('Valor depositado completo neste mês R$ {:.2f}'.format(pag))
 exce = pag - liquido
 print('O valor excedente do salario liquido é de R$ {:.2f}'.format(exce))
