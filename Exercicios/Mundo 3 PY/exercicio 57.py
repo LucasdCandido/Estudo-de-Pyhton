29a2b4643980b2d28c600c06629392df1bed3bec
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-09-03 23:51:21 -0300

    tentativa de resolução de exercicio de dicionario

--- Exercicios/Mundo 3 PY/exercicio 57.py
+++ Exercicios/Mundo 3 PY/exercicio 57.py
@@ -1,9 +1,17 @@
+from datetime import date
+cadastro = {}
+resposta = ""
+idade = nascimento = 0
+
+while True:
+    cadastro['Nome'] = input('Digite seu nome: ')
+    nascimento = int
 
 
 
 
 """Crie um programa que leia nomes, ano de nascimento e carteira de trabalho e cadastre-os(com idade) em um
-dicionario se por acado a CTPS for diferente de zero, o dicionario recebera tam´bem o ano de contratação e o salario.
+dicionario se por acado a CTPS for diferente de zero, o dicionario recebera também o ano de contratação e o salario.
 Calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
 35 anos de contribuição
 """
