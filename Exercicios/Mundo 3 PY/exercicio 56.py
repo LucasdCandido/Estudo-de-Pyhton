309c7780f4366b4dfdb9c5f59f440de18665a466
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-09-03 23:27:56 -0300

    Resolução de exercicio de dicionario

--- Exercicios/Mundo 3 PY/exercicio 56.py
+++ Exercicios/Mundo 3 PY/exercicio 56.py
@@ -1,3 +1,38 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+'''partidas = {}
+rank = {}
+jogos = []
+n = 0
+partidas['Jogador'] = 'Jogador 1'
+partidas['Dado'] = randint(1,6)
+jogos.append(partidas.copy())
+partidas['Jogador'] = 'Jogador 2'
+partidas['Dado'] = randint(1,6)
+jogos.append(partidas.copy())
+partidas['Jogador'] = 'Jogador 3'
+partidas['Dado'] = randint(1,6)
+jogos.append(partidas.copy())
+partidas['Jogador'] = 'Jogador 4'
+partidas['Dado'] = randint(1,6)
+jogos.append(partidas.copy())
+for c in range(0,4):
+    if c == 0 or c['Dado'] < rank[-1]:
+        rank.append(c)'''
+jogo = { 'Jogador1': randint(1,6),
+         'Jogador2': randint(1,6),
+         'Jogador3': randint(1,6),
+         'Jogador4': randint(1,6)}
+ranking = {}
+print('Jogos sorteados')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print(ranking)
+for i, v in enumerate(ranking):
+    print(f'{i+1} lugar: {v[0]} com {v[1]}.')
+    sleep(1)
 
 
 
