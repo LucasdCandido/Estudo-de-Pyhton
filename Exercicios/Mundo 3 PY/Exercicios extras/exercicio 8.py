5c96ea425b0a7db8acf727eb3a2634ecff00f6e3
Lucas Dourado <lucasdcandido1994@gmail.com>
2021-09-10 16:58:10 -0300

    tentativa de resolução

--- Exercicios/Mundo 3 PY/Exercicios extras/exercicio 8.py
+++ Exercicios/Mundo 3 PY/Exercicios extras/exercicio 8.py
@@ -1,13 +1,15 @@
 pessoa = []
 nome = []
+cadastro = []
 altura = []
 idade = []
 for c in range(0,2):
     nome.append(input('Nome: ').upper().strip())
     altura.append(float(input('Altura: ')))
     idade.append(int(input('Idade: ')))
-    pessoa.append([nome[:], altura[:], idade[:]])
-    nome.clear()
-    altura.clear()
-    idade.clear()
-print(pessoa)
+    pessoa.append(nome[:])
+    pessoa.append(altura[:])
+    pessoa.append(idade[:])
+    cadastro.append(pessoa[:])
+    pessoa.clear()
+print(cadastro)
