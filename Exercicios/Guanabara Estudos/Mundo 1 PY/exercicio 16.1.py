fd01944ed2fbccbf18a17c74462dda0a731c06d2
LucasdCandido <68237609+LucasdCandido@users.noreply.github.com>
2021-10-04 12:25:19 -0300

    correção e exercicios

---
+++ Exercicios/Guanabara Estudos/Mundo 1 PY/exercicio 16.1.py
@@ -0,0 +1,5 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+pygame.mixer.Sound.play(pygame.mixer.Sound('c:/Users/PC-OPERAÇÃO-LUCAS/Documents/GitHub/Estudo-de-Pyhton/Exercicios/Guanabara Estudos/Mundo 1 PY/Mundo_1_PY/teste.mp3'))
