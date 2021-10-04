bf4610bbd7b60189ff7bd508e7b9891208dc38bd
LucasdCandido <68237609+LucasdCandido@users.noreply.github.com>
2021-10-04 13:54:35 -0300

    tentando resolver problemas com pygame

--- Exercicios/Guanabara Estudos/Mundo 1 PY/exercicio 16.1.py
+++ Exercicios/Guanabara Estudos/Mundo 1 PY/exercicio 16.1.py
@@ -1,5 +1,7 @@
+from mutagen.mp3 import MP3
 import pygame
-from pygame.locals import *
 
 pygame.init()
-pygame.mixer.Sound.play(pygame.mixer.Sound('c:/Users/PC-OPERAÇÃO-LUCAS/Documents/GitHub/Estudo-de-Pyhton/Exercicios/Guanabara Estudos/Mundo 1 PY/Mundo_1_PY/teste.mp3'))
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.mixer.music.load('C:/Users/PC-OPERAÇÃO-LUCAS/Documents/GitHub/Estudo-de-Pyhton/Exercicios/Guanabara Estudos/Mundo 1 PY/testeteste.wav')
+pygame.mixer.music.play()
---
+++ Exercicios/Guanabara Estudos/Mundo 1 PY/testeteste.py
@@ -0,0 +1,18 @@
+import pygame, sys, time
+from pygame.locals import*
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Sound!!")
+
+soundObj = pygame.mixer.Sound('C:/Users/PC-OPERAÇÃO-LUCAS/Documents/GitHub/Estudo-de-Pyhton/Exercicios/Guanabara Estudos/Mundo 1 PY/testeteste.wav')
+soundObj.play()
+time.sleep(1) #wait and let the sound play for X second
+soundObj.stop()
+
+while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
