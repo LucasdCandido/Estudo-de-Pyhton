bf413247ccd90da9e1536e7fc30e0d098a66fff5
LucasdCandido <68237609+LucasdCandido@users.noreply.github.com>
2021-10-06 16:38:39 -0300

    correções

--- Exercicios/Guanabara Estudos/Mundo 2 PY/Trabalho/Salario.py
+++ Exercicios/Guanabara Estudos/Mundo 2 PY/Trabalho/Salario.py
@@ -8,5 +8,12 @@ pag = float(input('Digite quanto foi pago: R$ '))
 print('Valor depositado completo neste mês R$ {:.2f}'.format(pag))
 exce = pag - liquido
 print('O valor excedente do salario liquido é de R$ {:.2f}'.format(exce))
-exceliquido = exce-506.1
-print('Valor excedente menos o VT R$ {:.2f}'.format(exceliquido))
+dia = int(input('Digite quantos dias de trabalho no mês: '))
+bu = float(input('Digite o valor da passagem do Bilhete Unico: '))
+bom = float(input('Digite o valor da passagem do Bilhete Bom: '))
+inte = float(input('Digite o valor da integração do Bilhete unico entre onibus e trem: '))
+valor_bu = (dia*(bu*2))+(dia*(inte*2))
+valor_bom = (dia*(bom*2))
+exceliquido = exce-(valor_bom+valor_bu)
+restante = pag - valor_bom - valor_bu
+print(f'O valor do VT ficou {valor_bu+valor_bom:.2f}, o valor no BU é {valor_bu:.2f} e o valor do BOM é {valor_bom:.2f},o salario é {restante:.2f} e o exedente é {exceliquido:.2f}')
