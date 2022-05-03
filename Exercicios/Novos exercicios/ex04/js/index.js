11e0d8578be11e465c6713a82fde0ef36106a43c
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2022-05-03 19:58:30 -0300

    Correção de problema com js no projeto do DV

--- Exercicios/Novos exercicios/ex04/js/index.js
+++ Exercicios/Novos exercicios/ex04/js/index.js
@@ -11,19 +11,19 @@ function fillCounter() {
         getData('planets')
     ])
     .then(data => {
-        /*persons.style.fontSize = '5em';
+        persons.style.fontSize = '5em';
         starships.style.fontSize = '5em';
-        planets.style.fontSize = '5em';*/
+        planets.style.fontSize = '5em';
 
         persons.innerHTML = data[0].count;
         starships.innerHTML = data[1].count;
         planets.innerHTML = data[2].count;
-    })
+        })
     .catch(err => console.log('Erro:', err))
 }
 
 function getData(param) {
-    return fetch('https://swapi.dev/api/' + (param))
+    return fetch(`https://swapi.dev/api/${param}`)
             .then(res => res.json())
 }
 
@@ -33,9 +33,9 @@ function loadPhrase() {
 
     return fetch('http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote')
             .then(data => data.json())
-            .then(json =>{
+            .then(json => {
                 btn.innerHTML = 'Ver mais uma frase!';
-                phrase.innerHTML =  json.content;
+                phrase.innerHTML =  `"${json.content}"`;
 
                 phrase.animate([
                     {transform: 'translateY(-70px'},
