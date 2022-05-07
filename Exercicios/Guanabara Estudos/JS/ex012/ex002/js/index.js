25a371dd58a93da3c92c1eff4fb724cdf8f18401
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2022-05-07 15:02:34 -0300

    exercicio 2 feito e corrigido

--- Exercicios/Guanabara Estudos/JS/ex012/ex002/js/index.js
+++ Exercicios/Guanabara Estudos/JS/ex012/ex002/js/index.js
@@ -1,14 +1,37 @@
-var i = window.document.getElementById('idade')
-var m = window.document.getElementById('masculino')
-var f = window.document.getElementById('feminino')
-var corpo = window.document.getElementById('corpo')
-var dia = new Date()
-var ano = dia.getFullYear()
-var nasc = ano - i
+
 function carregar() {
-    if (i < 1940 || i > 2022) {
-        window.alert('Você digitou um ano de nascimento invalido, por favor digite de novo')
+    var fano = window.document.getElementById('idade')
+    var dia = new Date()
+    var ano = dia.getFullYear()
+    if (fano.value.length == 0 && Number(fano.value) > ano) {
+        window.alert('Você digitou um ano errado, digite novamente')
     } else {
-        resIdade.innerHTML = `Você tem ${nasc} anos`
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 20) {
+                img.setAttribute('src', 'img/crianca-menino.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'img/adulto-homem.png')
+            } else {
+                img.setAttribute('src', 'img/idoso.png')
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 20) {
+                img.setAttribute('src', 'img/crianca-menina.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'img/adulto-mulher.png')
+            } else {
+                img.setAttribute('src', 'img/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos. <br>`
+        res.appendChild(img)
     }
 }
