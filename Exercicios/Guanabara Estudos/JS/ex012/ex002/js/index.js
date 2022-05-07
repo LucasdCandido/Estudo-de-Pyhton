1efd13cfcae8e15f57daaa4a9beaa3f97f301100
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2022-05-07 13:11:44 -0300

    correção de imagens

--- Exercicios/Guanabara Estudos/JS/ex012/ex002/js/index.js
+++ Exercicios/Guanabara Estudos/JS/ex012/ex002/js/index.js
@@ -1,3 +1,14 @@
-var idaide = window.document.getElementById('idade')
+var i = window.document.getElementById('idade')
 var m = window.document.getElementById('masculino')
 var f = window.document.getElementById('feminino')
+var corpo = window.document.getElementById('corpo')
+var dia = new Date()
+var ano = dia.getFullYear()
+var nasc = ano - i
+function carregar() {
+    if (i < 1940 || i > 2022) {
+        window.alert('Você digitou um ano de nascimento invalido, por favor digite de novo')
+    } else {
+        resIdade.innerHTML = `Você tem ${nasc} anos`
+    }
+}
