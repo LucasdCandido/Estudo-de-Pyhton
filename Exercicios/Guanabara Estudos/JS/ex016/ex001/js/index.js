2f628ec36fdd4a12ef42fd23d3e8cad2651d5b21
LucasdCandido <lucasdcandido1994@gmail.com>
2022-05-12 15:01:13 -0300

    novos testes

--- Exercicios/Guanabara Estudos/JS/ex016/ex001/js/index.js
+++ Exercicios/Guanabara Estudos/JS/ex016/ex001/js/index.js
@@ -4,8 +4,20 @@ function enviar() {
      if (num.value.length == 0 || num.value == 0 || num.value > 100){
         window.alert('Numero invalido! Digite um numero entre 1 e 100')
     } else {
+        let n = Number(num.value)
         let item = document.createElement('option')
-        item.append(num)
+        item.append(n)
         sel.appendChild(item)
     }
+    
+}
+function checar(){
+    let num = document.getElementById('txtn')
+    let sel = document.getElementById('selnum')
+    if (sel.option[sel.selectedAllIndex].Number == 0){
+        window.alert('Digite algum numero primeiro!')
+    } else {
+        alert('click');
+    }
+    // alert("clicou");
 }
