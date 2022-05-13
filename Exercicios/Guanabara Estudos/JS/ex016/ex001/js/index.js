e67c9ad6ceab5ab7faa0659ea39415e98bd6cf6a
LucasdCandido <lucasdcandido1994@gmail.com>
2022-05-13 13:03:05 -0300

    correção do exercicio

--- Exercicios/Guanabara Estudos/JS/ex016/ex001/js/index.js
+++ Exercicios/Guanabara Estudos/JS/ex016/ex001/js/index.js
@@ -1,23 +1,10 @@
+let num = document.querySelector('input#txtn')
+let lista = document.querySelector('select#selnum')
+let res = document.querySelector('div#res')
+let valores = []
+
 function enviar() {
-    let num = document.getElementById('txtn')
-    let sel = document.getElementById('selnum')
-     if (num.value.length == 0 || num.value == 0 || num.value > 100){
-        window.alert('Numero invalido! Digite um numero entre 1 e 100')
-    } else {
-        let n = Number(num.value)
-        let item = document.createElement('option')
-        item.append(n)
-        sel.appendChild(item)
+    if (isNumber(num.value) && !inLista(num.value)){
+
     }
-    
-}
-function checar(){
-    let num = document.getElementById('txtn')
-    let sel = document.getElementById('selnum')
-    if (sel.option[sel.selectedAllIndex].Number == 0){
-        window.alert('Digite algum numero primeiro!')
-    } else {
-        alert('click');
-    }
-    // alert("clicou");
 }
